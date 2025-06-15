b2d0bb9bd11e0c5a676ec12df27ff30efc3c62eb
barhouum7 <bb.med2000@gmail.com>
2025-06-16 00:45:37 +0100

    Add MapContainerWrapContent component for improved map functionality. This new component integrates dynamic loading of the Map component, manages loading states, and supports fullscreen mode, enhancing user experience and visual feedback during interactions.

---
+++ src/components/map/map-container.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Filter, Loader2, Maximize2, RefreshCw } from 'lucide-react';
+import { Skeleton } from '../ui/skeleton';
+import { useState } from 'react';
+import { Button } from '../ui/button';
+import MapFilters from './map-filters';
+import { useMapState } from '@/hooks/use-map-state';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { cn } from '@/lib/utils';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ReliefLocationType } from '@/types/map';
+import Map from './map';
+
+export default function MapContainerWrapContent() {
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const [isRefreshing, setIsRefreshing] = useState(false);
+    const [isFullscreen, setIsFullscreen] = useState(false);
+    const [showFilter, setShowFilter] = useState(false);
+
+    const handleRefresh = async () => {
+        setIsRefreshing(true);
+        // Simulate data refresh
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        setIsRefreshing(false);
+    };
+
+    // const {
+    //     locations,
+    //     selectedLocation,
+    //     loading,
+    //     error,
+    //     activeFilters,
+    //     setSelectedLocation,
+    //     setActiveFilters,
+    //     // refreshData,
+    // } = useMapState();
+
+
+    return (
+        <>
+            {/* Map Section */}
+            <Card 
+            // className={`h-full p-0 relative shadow-lg border border-muted ${isFullscreen ? 'fixed inset-0 z-50' : ''}`}
+            className={`
+                h-full p-0
+                ${isFullscreen ? 'fixed inset-0 z-50 w-full bg-transparent border-none shadow-none' : 'shadow-lg border border-muted'}
+            `}
+            >
+                <div className={`${isFullscreen ? 'h-screen' : 'h-full'}`}>
+                    <Map 
+                        isFullscreen={isFullscreen}
+                        setIsFullscreen={setIsFullscreen}
+                    />
+                </div>
+            </Card>
+        </>
+    );
+}
