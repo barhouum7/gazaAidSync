effcbb3b4d49aea2ab0e5727731b885b93d92968
barhouum7 <bb.med2000@gmail.com>
2025-06-15 11:53:35 +0100

    Replace MapContainer with MapContainerWrapper in Home component for improved layout integration. This change enhances the structure of the MainGrid while maintaining the overall functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import MainGrid from "@/components/layout/main-grid";
 import UpdatesAndActions from "@/components/layout/updates-and-actions";
-import MapContainer from "@/components/map/map-container";
+import { MapContainerWrapper } from "@/components/map/map-container-wrapper";
 // import MapSidebar from "@/components/map/map-sidebar";
-
 
 export default function Home() {
     return (
         <MainGrid
             // left={<MapSidebar />}
-            center={<MapContainer />}
+            center={<MapContainerWrapper />}
             right={<UpdatesAndActions />}
         />
     );
