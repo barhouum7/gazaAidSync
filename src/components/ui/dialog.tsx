8bf71e826aa13ca7513e50d6614fa419be62d636
barhouum7 <bb.med2000@gmail.com>
2025-06-16 23:24:07 +0100

    Update DialogContent component to enhance close button styling. Changed button from rounded-xs to rounded-md, added cursor pointer, and improved hover and active states for better user interaction.

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -63,7 +63,9 @@ function DialogContent({
         {...props}
       >
         {children}
-        <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+        <DialogPrimitive.Close className="cursor-pointer ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-md opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4
+        p-1 hover:bg-accent hover:text-accent-foreground active:bg-accent/80 active:text-accent-foreground disabled:bg-muted disabled:text-muted-foreground
+        ">
           <XIcon />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
